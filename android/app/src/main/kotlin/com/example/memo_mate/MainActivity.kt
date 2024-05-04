31cc231b359b9fe5dfdd0316f0adf71aad4f87b3
Assil <astradhdbjd@gmail.com>
2024-05-04 22:21:20 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/memo_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.memo_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+
+}
+
+
+class MemoMate extends StatelessWidget {
+  const MemoMate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
