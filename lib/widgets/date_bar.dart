509411579f181ce6d2580c121949e6ad0b55c199
Assil <astradhdbjd@gmail.com>
2024-05-05 14:54:59 +0100

    add the active dateCard selection with the cubit

--- lib/widgets/date_bar.dart
+++ lib/widgets/date_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:memo_mate/cubits/cubit/date_cubit.dart';
 import 'package:memo_mate/helpers/get_dates.dart';
 import 'package:memo_mate/models/date_modal.dart';
 import 'package:memo_mate/widgets/date_show_card.dart';
@@ -9,16 +11,27 @@ class DateBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Date> dates = getNextTwoWeeks();
-    return SizedBox(
-      height: 60,
-      child: ListView.builder(
-        itemCount: dates.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return DateShowCard(
-            date: dates[index],
-          );
-        },
+    return BlocProvider(
+      create: (context) => DateCubit(),
+      child: SizedBox(
+        height: 60,
+        child: ListView.builder(
+          itemCount: dates.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return BlocBuilder<DateCubit, DateState>(
+              builder: (context, state) {
+                return DateShowCard(
+                  date: dates[index],
+                  index: index,
+                  color: BlocProvider.of<DateCubit>(context).current == index
+                      ? Colors.white
+                      : Colors.transparent,
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
--- lib/widgets/date_show_card.dart
+++ lib/widgets/date_show_card.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:memo_mate/cubits/cubit/date_cubit.dart';
 import 'package:memo_mate/models/date_modal.dart';
 import 'package:memo_mate/widgets/custom_text.dart';
 
 class DateShowCard extends StatelessWidget {
   final Date date;
   final Color color;
+  final int index;
   const DateShowCard({
     super.key,
     required this.date,
-    this.color = Colors.transparent,
+    required this.color, required this.index,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        BlocProvider.of<DateCubit>(context).selectDateCard(index);
+      },
       child: Container(
         padding: const EdgeInsets.all(8),
         margin: const EdgeInsets.only(right: 7),
