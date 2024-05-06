a14f4c8f7b642fb94413243c692f43629f1fd1bb
Assil <astradhdbjd@gmail.com>
2024-05-06 16:47:49 +0100

    create the select tag function and add tags

--- lib/cubits/tags_cubit/tags_cubit.dart
+++ lib/cubits/tags_cubit/tags_cubit.dart
@@ -1,9 +1,23 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'tags_state.dart';
 
 class TagsCubit extends Cubit<TagsState> {
   TagsCubit() : super(TagsInitial());
+
+  int current = 0;
+  List<String> tags = [
+    'All',
+    'Important',
+    'Entertainment',
+    'To-do lists',
+    'Shopping list',
+    'Personal',
+    'Others'
+  ];
+
+  void selectTagItem(int index) {
+    current = index;
+    emit(TagSelected());
+  }
 }
