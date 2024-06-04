9e91b398788a4e68897db5d17c324ce9ee4a1987
Assil <astradhdbjd@gmail.com>
2024-06-04 19:25:30 +0100

    create home view body

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:memo_mate/features/home/presentation/views/widgets/custom_search_bar.dart';
-import 'package:memo_mate/features/home/presentation/views/widgets/date_bar.dart';
-import 'package:memo_mate/features/home/presentation/views/widgets/empty_notes_view.dart';
-import 'package:memo_mate/features/home/presentation/views/widgets/home_view_app_bar.dart';
-import 'package:memo_mate/features/home/presentation/views/widgets/notes_container.dart';
-import 'package:memo_mate/features/home/presentation/views/widgets/tags_bar.dart';
+import 'package:memo_mate/features/home/presentation/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -13,38 +7,13 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const  EdgeInsets.only(
-          left: 18.0,
-          right: 18,
-          top: 22,
-        ),
-        child: NestedScrollView(
-          physics: const BouncingScrollPhysics(),
-          headerSliverBuilder:
-              (BuildContext context, bool innerBoxIsScrolled) => [
-            const SliverToBoxAdapter(
-              child: Column(
-                children: [
-                  HomeViewAppBar(),
-                  SizedBox(height: 17),
-                  CustomSearchBar(),
-                  SizedBox(height: 17),
-                  DateBar(),
-                  SizedBox(height: 17),
-                  TagsBar(),
-                  SizedBox(height: 17),
-                ],
-              ),
-            ),
-          ],
-          body: const NotesContainer(),
-
-          // SliverToBoxAdapter(
-          //   child: NotesContainer(),
-          // )
-        ),
-      ),
+      body: const Padding(
+          padding: EdgeInsets.only(
+            left: 18.0,
+            right: 18,
+            top: 22,
+          ),
+          child: HomeViewBody()),
       floatingActionButton: FloatingActionButton(
         shape: const CircleBorder(),
         backgroundColor: Colors.white,
---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:memo_mate/features/home/presentation/views/widgets/custom_search_bar.dart';
+import 'package:memo_mate/features/home/presentation/views/widgets/date_bar.dart';
+import 'package:memo_mate/features/home/presentation/views/widgets/home_view_app_bar.dart';
+import 'package:memo_mate/features/home/presentation/views/widgets/notes_container.dart';
+import 'package:memo_mate/features/home/presentation/views/widgets/tags_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+      physics: const BouncingScrollPhysics(),
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) => [
+        const SliverToBoxAdapter(
+          child: Column(
+            children: [
+              HomeViewAppBar(),
+              SizedBox(height: 17),
+              CustomSearchBar(),
+              SizedBox(height: 17),
+              DateBar(),
+              SizedBox(height: 17),
+              TagsBar(),
+              SizedBox(height: 17),
+            ],
+          ),
+        ),
+      ],
+      body: const NotesContainer(),
+    );
+  }
+}
