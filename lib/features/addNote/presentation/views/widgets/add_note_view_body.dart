5fa74539aa595e9da4e5f1ec3334c359e7f0d813
Assil <astradhdbjd@gmail.com>
2024-06-09 21:38:08 +0100

    image placeholder part 1

--- lib/features/addNote/presentation/views/widgets/add_note_view_body.dart
+++ lib/features/addNote/presentation/views/widgets/add_note_view_body.dart
@@ -1,5 +1,5 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:memo_mate/features/addNote/presentation/views/widgets/add_note_app_bar.dart';
 import 'package:memo_mate/features/addNote/presentation/views/widgets/custom_tool_bar.dart';
 
@@ -24,18 +24,38 @@ class AddNoteViewBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 24),
             child: CustomToolBar(),
           ),
+          ImagePlaceholder(),
         ],
       ),
     );
   }
 }
 
-
-class ImagePlaceHolder extends StatelessWidget {
-  const ImagePlaceHolder({super.key});
+class ImagePlaceholder extends StatelessWidget {
+  const ImagePlaceholder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      color: Theme.of(context).colorScheme.primary.withAlpha(155),
+      radius: const Radius.circular(12),
+      dashPattern: const [22, 12],
+      child: Container(
+        height: MediaQuery.of(context).size.height * .28,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Theme.of(context).colorScheme.background.withOpacity(0.4),
+        ),
+        child: const Column(
+          children: [
+            CircleAvatar(
+              radius: 27,
+              child: Icon(Icons.image),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
