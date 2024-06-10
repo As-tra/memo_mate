779522ca035de23c47fc561740adf17c64c9a884
Assil <astradhdbjd@gmail.com>
2024-06-10 21:18:23 +0100

    add close image button to the image placeholder

--- lib/features/addNote/presentation/views/widgets/image_placeholder.dart
+++ lib/features/addNote/presentation/views/widgets/image_placeholder.dart
@@ -1,11 +1,8 @@
 import 'dart:io';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:memo_mate/core/utils/assets.dart';
-import 'package:memo_mate/core/utils/styles.dart';
-import 'package:memo_mate/features/addNote/presentation/views/widgets/custom_button.dart';
+import 'package:memo_mate/features/addNote/presentation/views/widgets/empty_image_placeholder.dart';
 
 class ImagePlaceholder extends StatefulWidget {
   const ImagePlaceholder({super.key});
@@ -39,48 +36,33 @@ class _ImagePlaceholderState extends State<ImagePlaceholder> {
         radius: const Radius.circular(12),
         dashPattern: const [22, 12],
         child: _image == null
-            ? Container(
-                height: MediaQuery.of(context).size.height * .28,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color:
-                      Theme.of(context).colorScheme.background.withOpacity(0.4),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 28,
-                      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                      child: SvgPicture.asset(AssetsData.imagePlaceholder),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * .7,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 11.0),
-                            child: Text(
-                              'Upload an image,To you guidance can be clearer for you.',
-                              textAlign: TextAlign.center,
-                              style: Styles.textStyle14.copyWith(
-                                color: Theme.of(context).colorScheme.secondary,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const CustomBotton(),
-                  ],
-                ),
-              )
+            ? const EmptyImagePlaceholder()
             : ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                child: Image.file(
-                  File(_image!.path),
-                  fit: BoxFit.cover,
+                child: Stack(
+                  children: [
+                    Image.file(
+                      File(_image!.path),
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: MediaQuery.of(context).size.width * .5 - 33,
+                      child: CircleAvatar(
+                        radius: 22,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondary,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _image = null;
+                            });
+                          },
+                          icon: const Icon(Icons.close),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
       ),
