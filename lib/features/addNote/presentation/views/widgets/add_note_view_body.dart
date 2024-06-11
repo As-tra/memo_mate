79343abccc1fa1cd1997cf76b94ea014396ccaf5
Assil <astradhdbjd@gmail.com>
2024-06-11 23:44:04 +0100

    fix the add note page padding

--- lib/features/addNote/presentation/views/widgets/add_note_view_body.dart
+++ lib/features/addNote/presentation/views/widgets/add_note_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:memo_mate/core/constants.dart';
 import 'package:memo_mate/core/utils/styles.dart';
 import 'package:memo_mate/features/addNote/presentation/views/widgets/add_note_app_bar.dart';
@@ -14,75 +15,54 @@ class AddNoteViewBody extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         const AddNoteAppBar(),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              // const Padding(
-              //   padding: EdgeInsets.symmetric(horizontal: 12.0),
-              //   child: AddNoteAppBar(),
-              // ),
-              const Padding(
-                padding: EdgeInsets.all(20),
-                child: CustomToolBar(),
-              ),
-              const ImagePlaceholder(),
-              const SizedBox(height: 17),
-              CustomTextFormField(
-                hintText: 'Title',
-                style: Styles.textStyle18.copyWith(
-                  fontFamily: kComfortaa,
+        SliverPadding(
+          padding: const EdgeInsets.only(
+            left: 12,
+            right: 12,
+          ),
+          sliver: SliverToBoxAdapter(
+            child: Column(
+              children: [
+                // const Padding(
+                //   padding: EdgeInsets.symmetric(horizontal: 12.0),
+                //   child: AddNoteAppBar(),
+                // ),
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: CustomToolBar(),
                 ),
-              ),
-              const SizedBox(height: 6.3),
-              CustomTextFormField(
-                hintText: 'Notes...',
-                style: Styles.textStyle15.copyWith(
-                  fontWeight: FontWeight.w300,
+                const ImagePlaceholder(),
+                const SizedBox(height: 17),
+                CustomTextFormField(
+                  hintText: 'Title',
+                  style: Styles.textStyle18.copyWith(
+                    fontFamily: kComfortaa,
+                  ),
                 ),
-                maxlines: 4,
-              ),
-              CustomTextFormField(
-                hintText: 'Notes...',
-                style: Styles.textStyle15.copyWith(
-                  fontWeight: FontWeight.w300,
+                const SizedBox(height: 6.3),
+                CustomTextFormField(
+                  hintText: 'Notes...',
+                  style: Styles.textStyle15.copyWith(
+                    fontWeight: FontWeight.w300,
+                  ),
+                  maxlines: 4,
                 ),
-                maxlines: 4,
-              ),
-              CustomTextFormField(
-                hintText: 'Notes...',
-                style: Styles.textStyle15.copyWith(
-                  fontWeight: FontWeight.w300,
+
+                const SizedBox(height: 13.5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Last Changed : Mar 1, 2024 at 2.00AM',
+                      style: Styles.textStyle12.copyWith(
+                        color: Colors.white.withOpacity(0.6),
+                        fontWeight: FontWeight.w300,
+                      ),
+                    )
+                  ],
                 ),
-                maxlines: 4,
-              ),
-              CustomTextFormField(
-                hintText: 'Notes...',
-                style: Styles.textStyle15.copyWith(
-                  fontWeight: FontWeight.w300,
-                ),
-                maxlines: 4,
-              ),
-              CustomTextFormField(
-                hintText: 'Notes...',
-                style: Styles.textStyle15.copyWith(
-                  fontWeight: FontWeight.w300,
-                ),
-                maxlines: 4,
-              ),
-              const SizedBox(height: 13.5),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Last Changed : Mar 1, 2024 at 2.00AM',
-                    style: Styles.textStyle12.copyWith(
-                      color: Colors.white.withOpacity(0.6),
-                      fontWeight: FontWeight.w300,
-                    ),
-                  )
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
