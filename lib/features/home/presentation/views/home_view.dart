0af6c3b65c0eda44dd724cfd0a8536c1fc4f5819
Assil <astradhdbjd@gmail.com>
2024-06-12 21:44:49 +0100

    change the button animation to pulse animation

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lottie/lottie.dart';
 import 'package:memo_mate/core/utils/app_router.dart';
 import 'package:memo_mate/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:pulsator/pulsator.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -18,22 +18,20 @@ class HomeView extends StatelessWidget {
         ),
         child: HomeViewBody(),
       ),
-      floatingActionButton: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Lottie.asset('assets/animations/wave.json'),
-          ),
-          const Positioned(
-            bottom: 26,
-            right: 27,
-            child: Icon(
-              Icons.add,
-              color: Colors.black,
-              size: 32,
-            ),
-          ),
-        ],
+      floatingActionButton: GestureDetector(
+        onTap: () {
+          GoRouter.of(context).push(AppRouter.kAddNoteView);
+        },
+        child: const PulseIcon(
+          icon: Icons.add,
+          pulseColor: Colors.white,
+          iconColor: Colors.black,
+          iconSize: 32,
+          innerSize: 50,
+          pulseSize: 120,
+          pulseCount: 2,
+          pulseDuration: Duration(seconds: 4),
+        ),
       ),
     );
   }
