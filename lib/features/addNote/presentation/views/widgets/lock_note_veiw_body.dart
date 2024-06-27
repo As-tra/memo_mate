b5a87260f84675c6b88819fb6d3594bc20918a22
Assil <astradhdbjd@gmail.com>
2024-06-27 21:28:24 +0100

    pin code fields part 1

--- lib/features/addNote/presentation/views/widgets/lock_note_veiw_body.dart
+++ lib/features/addNote/presentation/views/widgets/lock_note_veiw_body.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:memo_mate/core/utils/styles.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class LockNoteViewBody extends StatelessWidget {
   const LockNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const Text(
+          'Enter Your PIN',
+          style: Styles.textStyle22,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 45.0),
+          child: _createPinCodeFields(context),
+        ),
+      ],
+    );
+  }
+
+  PinCodeTextField _createPinCodeFields(BuildContext context) {
+    return PinCodeTextField(
+      appContext: context,
+      length: 5,
+      animationType: AnimationType.fade,
+      pinTheme: PinTheme(
+        selectedFillColor: Colors.white,
+        inactiveFillColor: Colors.transparent,
+        activeFillColor: Theme.of(context).colorScheme.primary,
+        activeColor: Colors.black,
+        inactiveColor: Colors.white,
+        shape: PinCodeFieldShape.circle,
+        fieldHeight: 52,
+        fieldWidth: 52,
+      ),
+      animationDuration: const Duration(milliseconds: 300),
+      enableActiveFill: true,
+      onCompleted: (value) {
+        print(value);
+      },
+      onChanged: (value) {},
+      beforeTextPaste: (text) {
+        return true;
+      },
+    );
   }
 }
